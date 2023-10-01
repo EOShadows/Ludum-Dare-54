5604eaa31f941a3854d2c53d602f29b621ae053e
Matthew Sturgeon <96439614+mattsturg@users.noreply.github.com>
2023-10-01 16:32:59 -0600

    weaponFix

--- LDGame/Assets/weaponAttack.cs
+++ LDGame/Assets/weaponAttack.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class weaponAttack : MonoBehaviour
 {
     Animator anim;
+    public float angle = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,11 +17,17 @@ public class weaponAttack : MonoBehaviour
     {
         Vector2 positionOnScreen = Camera.main.WorldToViewportPoint (transform.position);
 		Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
-		float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
-        transform.parent.eulerAngles = new Vector3(0f, 0f, angle-90f);
+		angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
+
+        // angle = Mathf.Round(angle / 45) * 45;
+
+		transform.rotation =  Quaternion.Euler (new Vector3(0f,0f,angle+90f));
+        
         // transform.RotateAround(transform.parent.position, Vector3.forward, angle);
         if(Input.GetButtonDown("Fire1")){
             anim.SetTrigger("attack");
+            Debug.Log(positionOnScreen);
+            Debug.Log(mouseOnScreen);
         }
     }
 
