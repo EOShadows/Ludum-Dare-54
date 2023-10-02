307e565ba6c2d3c6511aa8f6ff3d4e8dd150e2ba
Matthew Sturgeon <96439614+mattsturg@users.noreply.github.com>
2023-10-01 18:50:32 -0600

    flash when hurt

--- LDGame/Assets/Scripts/health.cs
+++ LDGame/Assets/Scripts/health.cs
@@ -7,14 +7,38 @@ public class health : MonoBehaviour
 {
     float currHealth;
     public float maxHealth;
+    float maxFlashTime = 0.2f;
+    float currFlashTime;
+    bool tookDamage = false;
+    public Color hurtColor;
+    Color mainColor;
+    SpriteRenderer rend;
     // Start is called before the first frame update
     void Start()
     {
         currHealth = maxHealth;
+        currFlashTime = maxFlashTime;
+        rend = GetComponent<SpriteRenderer>();
+        mainColor = rend.color;
+    }
+
+    void Update(){
+        if(tookDamage){
+            currFlashTime -= Time.deltaTime;
+            if(currFlashTime <= 0f){
+                tookDamage = false;
+                rend.color = mainColor;
+                currFlashTime = maxFlashTime;
+            }
+        }
     }
 
     public void takeDamage(float damage){
         currHealth -= damage;
+
+        tookDamage = true;
+        rend.color = hurtColor;
+
         if(currHealth <= 0f){
             handleDeath();
         }
