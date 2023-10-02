22ac8e61a27a0e6637685bcfba26e853e3ad7517
Alex O'Hara <awostorm@gmail.com>
2023-10-02 13:07:39 -0600

    fixed strange issue with hatman script

--- LDGame/Assets/Scripts/hatman_behaviour.cs
+++ LDGame/Assets/Scripts/hatman_behaviour.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class hatman_behaviour : MonoBehaviour
+public class Hatman_Behaviour : MonoBehaviour
 {
     private Animator anime;
     private simpleEnemyMove move;
